037f73920f3d4e914cde3f55f87e7c6b0dc16c5d
Mahmoud Ashraf <mahmoud.ashraf.cis@gmail.com>
2022-02-28 01:09:36 +0200

    Update SQLiteExample.cs

--- Assets/Scripts/SQLiteExample.cs
+++ Assets/Scripts/SQLiteExample.cs
@@ -4,21 +4,63 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mono.Data.Sqlite;
 using System.Data;
+using System.Linq;
 
+public class student
+{
+    public int i;
+    public string name;
+
+}
 public class SQLiteExample : MonoBehaviour
 {
     // Start is called before the first frame update
     private string dbName;
-    
+    public student ss;
+    public Time t;
+    public int[] arr;
 
     void Start()
     {
         dbName = "URI=file:" + Application.persistentDataPath + "/Inventory.db";
         Debug.Log(dbName);
-        CreateDB();
-        AddUser("ali@ggf.com","somePassword","ali","ahmed");
-        AddUser("nanny@ggf.com","somePassword1111","ahmed","ashraf");
-        PrintUsers();
+
+        student s = new student();
+        //System.Reflection.PropertyInfo[] infs = typeof(student).GetType().GetProperties();
+        //Debug.Log(infs.Length);
+        //foreach (var i in infs)
+        //{
+        //    Debug.Log(i.Name + "-----------------" + i.GetType());
+        //}
+
+        //System.Reflection.FieldInfo[] ffinfo = typeof(student).GetFields();
+        //foreach (var inf in ffinfo)
+        //{
+        //    Debug.Log(inf.FieldType + " ----- " + inf.Name);
+        //}
+
+
+        Debug.Log(s.GetSQLTable());
+        Debug.Log(this.GetSQLTable());
+
+
+
+
+        //var fieldNames = typeof(student).GetFields()
+        //                    .Select(field => field.Name)
+        //                    .ToList();
+
+        //var fieldTypes = typeof(student).GetFields()
+        //                    .Select(field => field.GetType().ToString())
+        //                    .ToList();
+        //for(int i=0;i<fieldNames.Count; i++) { 
+        //    Debug.Log(fieldTypes[i] + " ###### "+fieldNames[i]);
+        //}
+
+        //CreateDB();
+        //AddUser("ali@ggf.com","somePassword","ali","ahmed");
+        //AddUser("nanny@ggf.com","somePassword1111","ahmed","ashraf");
+        //PrintUsers();
     }
 
     private void PrintUsers()
@@ -111,6 +153,18 @@ public static class Extension
     public static string EncSingleQoute(this string str)
     {
         return "'" + str + "'";
+    }
+
+    public static string GetSQLTable(this object o)
+    {
+        string s = "";
+        System.Reflection.FieldInfo[] ffinfo = o.GetType().GetFields();
+        foreach (var inf in ffinfo)
+        {
+            s+= inf.FieldType + " ----- " + inf.Name + "\n";
+        }
+
+        return s;
     }
  
 }
